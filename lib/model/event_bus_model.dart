db1173040ab76a168d85acc05a3ed5630c96061b
kafka <kafka@yunio.com>
2020-08-25 14:26:52 +0800

    add event bus demo

---
+++ lib/model/event_bus_model.dart
@@ -0,0 +1,8 @@
+
+class EventBusModel {
+  final String title;
+  final String desc;
+  final int id;
+
+  EventBusModel({this.title, this.desc, this.id});
+}
--- lib/pages/event_bus_demo.dart
+++ lib/pages/event_bus_demo.dart
@@ -1,3 +1,4 @@
+import 'package:demo/model/event_bus_model.dart';
 import 'package:demo/util/index.dart';
 import 'package:flutter/material.dart';
 
@@ -22,6 +23,13 @@ class _EventBusDemoPageState extends State<EventBusDemoPage> {
           children: <Widget>[
             _ButtonDemo(),
             _TextDemo(),
+            RaisedButton(
+              onPressed: () {
+                eventBus.fire(EventBusModel(title: 'eventBusModel', desc: 'desc', id: 1));
+              },
+              child: Text('发送model事件'),
+            ),
+            _EventBusModelDemo()
           ],
         ),
       ),
@@ -50,10 +58,10 @@ class _TextDemo extends StatefulWidget {
 class _TextDemoState extends State<_TextDemo> {
   String message = 'helloWorld';
 
-
   @override
   void initState() {
     super.initState();
+
     ///3: 监听事件的类型
     eventBus.on<String>().listen((event) {
       print(event);
@@ -63,5 +71,31 @@ class _TextDemoState extends State<_TextDemo> {
   @override
   Widget build(BuildContext context) {
     return Text("");
+  }
+}
+
+class _EventBusModelDemo extends StatefulWidget {
+  @override
+  _EventBusModelDemoState createState() => _EventBusModelDemoState();
+}
+
+class _EventBusModelDemoState extends State<_EventBusModelDemo> {
+  EventBusModel eventBusModel;
+
+  @override
+  void initState() {
+    super.initState();
+    eventBus.on<EventBusModel>().listen((event) {
+      setState(() {
+        eventBusModel = event;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('title = ${eventBusModel?.title}, desc = ${eventBusModel?.desc}, id = ${eventBusModel?.id}'),
+    );
   }
 }
