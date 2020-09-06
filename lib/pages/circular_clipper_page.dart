db18d9bbe7dcd4d7d67119b88150585b344c91ce
luoji <wuweijian1997@163.com>
2020-09-06 18:49:23 +0800

    1234

--- lib/pages/circular_clipper_page.dart
+++ lib/pages/circular_clipper_page.dart
@@ -3,20 +3,22 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class CircularClipperPage extends StatefulWidget {
-  static const rName = 'imgClipperPage';
+  static const rName = 'circularClipperPage';
 
   @override
   _CircularClipperPageState createState() => _CircularClipperPageState();
 }
 
-class _CircularClipperPageState extends State<CircularClipperPage> with TickerProviderStateMixin{
+class _CircularClipperPageState extends State<CircularClipperPage> with TickerProviderStateMixin {
   AnimationController animationController;
+  GlobalKey globalKey = GlobalKey();
 
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(duration: Duration(seconds: 3),vsync: this);
+    animationController = AnimationController(duration: Duration(seconds: 3), vsync: this);
     animationController.forward();
+
     ///监听动画的改变
     animationController.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
@@ -25,8 +27,10 @@ class _CircularClipperPageState extends State<CircularClipperPage> with TickerPr
         animationController.forward();
       }
     });
+    Future.delayed(Duration(seconds: 1), () {
+      print(globalKey.currentContext.size);
+    });
   }
-
 
   @override
   void dispose() {
@@ -40,33 +44,23 @@ class _CircularClipperPageState extends State<CircularClipperPage> with TickerPr
       appBar: AppBar(
         title: Text('CircularClipper'),
       ),
-      body: Column(
+      body: Stack(
         children: [
-          Stack(
-            children: [
-              Image.asset(
-                "assets/rem02.jpg",
-                width: 300,
-                height: 600,
-                fit: BoxFit.cover,
-              ),
-              AnimatedBuilder(
-                animation: animationController,
-                builder: (ctx, child) {
-                  return ClipOval(
-                    clipper: CircularClipper(percentage: animationController?.value, offset: const Offset(150, 300)),
-                    child: child,
-                  );
-                },
-                child: Image.asset(
-                  "assets/rem.jpg",
-                  width: 300,
-                  height: 600,
-                  fit: BoxFit.cover,
-
-                ),
-              ),
-            ],
+          Image.asset(
+            "assets/rem02.jpg",
+            key: globalKey,
+          ),
+          AnimatedBuilder(
+            animation: animationController,
+            builder: (ctx, child) {
+              return ClipOval(
+                clipper: CircularClipper(percentage: animationController?.value, offset: const Offset(100.7, 200.7)),
+                child: child,
+              );
+            },
+            child: Image.asset(
+              "assets/rem.jpg",
+            ),
           ),
         ],
       ),
@@ -75,6 +69,7 @@ class _CircularClipperPageState extends State<CircularClipperPage> with TickerPr
 }
 
 class CircularClipper extends CustomClipper<Rect> {
+  ///百分比, 0-> 1,1 => 全部显示
   final double percentage;
   final Offset offset;
 
@@ -83,12 +78,12 @@ class CircularClipper extends CustomClipper<Rect> {
   @override
   Rect getClip(Size size) {
     double maxValue = maxLength(size, offset) * percentage;
-    return Rect.fromLTRB(-maxValue + offset.dx, -maxValue + offset.dy,  maxValue+ offset.dx, maxValue+ offset.dy);
+    return Rect.fromLTRB(-maxValue + offset.dx, -maxValue + offset.dy, maxValue + offset.dx, maxValue + offset.dy);
   }
 
   @override
   bool shouldReclip(CircularClipper oldClipper) {
-    return percentage != oldClipper.percentage;
+    return percentage != oldClipper.percentage || offset != oldClipper.offset;
   }
 
   ///     |
@@ -99,13 +94,13 @@ class CircularClipper extends CustomClipper<Rect> {
   double maxLength(Size size, Offset offset) {
     double centerX = size.width / 2;
     double centerY = size.height / 2;
-    if(offset.dx < centerX && offset.dy < centerY) {
+    if (offset.dx < centerX && offset.dy < centerY) {
       ///1
       return getEdge(size.width - offset.dx, size.height - offset.dy);
-    } else if(offset.dx > centerX && offset.dy < centerY) {
+    } else if (offset.dx > centerX && offset.dy < centerY) {
       ///2
       return getEdge(offset.dx, size.height - offset.dy);
-    } else if(offset.dx <centerX && offset.dy > centerY) {
+    } else if (offset.dx < centerX && offset.dy > centerY) {
       ///3
       return getEdge(size.width - offset.dx, offset.dy);
     } else {
@@ -118,4 +113,3 @@ class CircularClipper extends CustomClipper<Rect> {
     return sqrt(pow(width, 2) + pow(height, 2));
   }
 }
-
