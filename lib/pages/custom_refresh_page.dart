7d6c7e78d1c6cfed9b114810227a622b033b5a4c
kafka <kafka@yunio.com>
2020-10-30 18:14:28 +0800

    custom sliver

--- lib/pages/custom_refresh_page.dart
+++ lib/pages/custom_refresh_page.dart
@@ -1,30 +1,20 @@
+
+import 'dart:math';
+
 import 'package:demo/util/index.dart';
 import 'package:demo/widgets/index.dart';
 import 'package:flutter/material.dart';
 
 class CustomRefreshPage extends StatefulWidget {
   static const String rName = 'CustomRefresh';
+
   @override
   _CustomRefreshPageState createState() => _CustomRefreshPageState();
 }
 
 class _CustomRefreshPageState extends State<CustomRefreshPage> {
-  bool hasLayoutExtent = true;
+  bool hasLayoutExtent = false;
   ScrollController scrollController;
-
-
-  @override
-  void initState() {
-    super.initState();
-    scrollController = ScrollController();
-    scrollController.addListener(() {
-    });
-    Future.delayed(Duration(milliseconds: 3000), () {
-      setState(() {
-        hasLayoutExtent = false;
-      });
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,17 +30,36 @@ class _CustomRefreshPageState extends State<CustomRefreshPage> {
           controller: scrollController,
           slivers: <Widget>[
             CustomRefreshWidget(
-                layoutExtent: 100.0,
+                refreshLayoutExtent: 60.0,
                 hasLayoutExtent: hasLayoutExtent,
-                child: Container(
-                  height: 100,
-                  alignment: Alignment.center,
-                  child: CustomLoading(),
+                child: LayoutBuilder(
+                  builder: (_, BoxConstraints constraints) {
+                    return Stack(
+                      children: [
+                        Positioned(
+                          top: null,
+                          left: 0,
+                          right: 0,
+                          bottom: 0,
+                          child: Container(
+                            height: max(60, constraints.maxHeight),
+                            alignment: Alignment.bottomCenter,
+                            child: Container(
+                              height: 60,
+                              width: double.infinity,
+                              alignment: Alignment.center,
+                              child: CustomLoading(),
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
                 )),
             SliverFixedExtentList(
               itemExtent: 50.0,
               delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) {
+                (BuildContext context, int index) {
                   //创建列表项
                   return Container(
                     alignment: Alignment.center,
--- lib/widgets/custom_refresh_widget.dart
+++ lib/widgets/custom_refresh_widget.dart
@@ -1,21 +1,25 @@
 import 'dart:math';
 
-import 'package:demo/util/index.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 class CustomRefreshWidget extends SingleChildRenderObjectWidget {
-  const CustomRefreshWidget(
-      {Key key, Widget child, this.hasLayoutExtent, this.layoutExtent})
-      : super(key: key, child: child);
+  const CustomRefreshWidget({
+    Key key,
+    Widget child,
+    this.hasLayoutExtent,
+    this.refreshLayoutExtent,
+  }) : super(key: key, child: child);
 
   final bool hasLayoutExtent;
-  final double layoutExtent;
+  final double refreshLayoutExtent;
 
   @override
   RenderObject createRenderObject(BuildContext context) {
     return SimpleRefreshSliver(
-        hasLayoutExtent: hasLayoutExtent, layoutExtent: layoutExtent);
+      hasLayoutExtent: hasLayoutExtent,
+      refreshLayoutExtent: refreshLayoutExtent,
+    );
   }
 
   @override
@@ -23,17 +27,17 @@ class CustomRefreshWidget extends SingleChildRenderObjectWidget {
       BuildContext context, covariant SimpleRefreshSliver renderObject) {
     renderObject
       ..hasLayoutExtent = hasLayoutExtent
-      ..layoutExtent = layoutExtent;
+      ..refreshLayoutExtent = refreshLayoutExtent;
   }
 }
 
 class SimpleRefreshSliver extends RenderSliverSingleBoxAdapter {
   bool _hasLayoutExtent;
-  double _layoutExtent;
+  double _refreshLayoutExtent;
 
-  SimpleRefreshSliver({bool hasLayoutExtent, double layoutExtent})
+  SimpleRefreshSliver({bool hasLayoutExtent, double refreshLayoutExtent})
       : _hasLayoutExtent = hasLayoutExtent,
-        _layoutExtent = layoutExtent;
+        _refreshLayoutExtent = refreshLayoutExtent;
 
   set hasLayoutExtent(bool value) {
     assert(value != null);
@@ -42,46 +46,53 @@ class SimpleRefreshSliver extends RenderSliverSingleBoxAdapter {
     markNeedsLayout();
   }
 
-  get hasLayoutExtent => _hasLayoutExtent;
-
-  double get layoutExtent => _layoutExtent;
-
-  set layoutExtent(double value) {
+  set refreshLayoutExtent(double value) {
     assert(value != null);
     assert(value > 0);
-    _layoutExtent = value;
+    _refreshLayoutExtent = value;
     markNeedsLayout();
   }
+
+  // 获取子组件大小
+  double get childSize => child.size.height;
+
+  double layoutExtentOffsetCompensation = 0.0;
 
   @override
   void performLayout() {
     final SliverConstraints constraints = this.constraints;
-    final bool active = constraints.overlap < 0.0;
+    final double layoutExtent = _hasLayoutExtent ? _refreshLayoutExtent : 0;
+    if (layoutExtentOffsetCompensation != layoutExtent) {
+      geometry = SliverGeometry(
+          scrollOffsetCorrection:
+              layoutExtent - layoutExtentOffsetCompensation);
+      layoutExtentOffsetCompensation = layoutExtent;
+      return;
+    }
+    final bool active = constraints.overlap < 0.0 || layoutExtent > 0.0;
 
     /// 头部滑动的距离
-    final double overScrolledExtent = active ? constraints.overlap.abs() : 0.0;
-    double layoutExtentSize = max(overScrolledExtent, layoutExtent);
+    final double overScrolledExtent = min(constraints.overlap, 0.0).abs();
 
     ///将 SliverConstraints转化为BoxConstraints对child进行layout
-    child.layout(constraints.asBoxConstraints(), parentUsesSize: true);
-    if (active || hasLayoutExtent) {
+    child.layout(
+        constraints.asBoxConstraints(
+          maxExtent: layoutExtent + overScrolledExtent,
+        ),
+        parentUsesSize: true);
+    if (active) {
+      final _layoutExtent =
+          max(max(childSize, layoutExtent) - constraints.scrollOffset, 0.0);
       geometry = SliverGeometry(
-        scrollExtent: 0,
-        maxPaintExtent: layoutExtentSize,
-
-        /// RefreshWidget 的绘制起始位置
-        paintOrigin: 0,
-        paintExtent: max(child.size.height, layoutExtentSize),
-
-        /// 布局占位
-        layoutExtent: hasLayoutExtent
-            ? layoutExtentSize
-            : min(overScrolledExtent, layoutExtentSize),
+        scrollExtent: layoutExtent,
+        paintExtent: min(_layoutExtent, constraints.remainingPaintExtent),
+        maxPaintExtent: _layoutExtent,
+        paintOrigin: -overScrolledExtent - constraints.scrollOffset,
+        layoutExtent: max(layoutExtent - constraints.scrollOffset, 0.0),
       );
     } else {
       /// 如果不想显示可以直接设置为 zero
       geometry = SliverGeometry.zero;
     }
-    setChildParentData(child, constraints, geometry);
   }
 }
