af5ec63bcf36d02b0ce98ca0d0fcb45a0866d152
kafka <kafka@yunio.com>
2021-03-11 15:12:54 +0800

    Canvas 文字绘制

--- lib/pages/canvas/basic/text_drawing.dart
+++ lib/pages/canvas/basic/text_drawing.dart
@@ -1,4 +1,9 @@
+import 'package:demo/model/index.dart';
+import 'package:demo/pages/index.dart';
+import 'package:demo/util/index.dart';
+import 'package:demo/widgets/index.dart';
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
 class TextDrawing extends StatefulWidget {
   @override
@@ -6,8 +11,76 @@ class TextDrawing extends StatefulWidget {
 }
 
 class _TextDrawingState extends State<TextDrawing> {
+  var list = <ListPageModel>[];
+
+  @override
+  void initState() {
+    list = [
+      ListPageModel(
+          title: "drawParagraph绘制文字",
+          page: BasicCustomPaint(drawTextWithParagraph)),
+      ListPageModel(
+          title: "TextPainter获取文字范围",
+          page: BasicCustomPaint(drawTextPaintShowSize)),
+    ];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListPage(list);
+  }
+
+  ///drawParagraph绘制文字
+  drawTextWithParagraph(Canvas canvas, Size size) {
+    canvas.translate((size.width - 300) / 2, (size.height - 100) / 2);
+    drawTextWithParagraphAlign(canvas, TextAlign.left);
+    canvas.translate(0, 50);
+    drawTextWithParagraphAlign(canvas, TextAlign.center);
+    canvas.translate(0, 50);
+    drawTextWithParagraphAlign(canvas, TextAlign.right);
+  }
+
+  drawTextWithParagraphAlign(Canvas canvas, TextAlign textAlign) {
+    var builder = ui.ParagraphBuilder(ui.ParagraphStyle(
+      textAlign: textAlign,
+      fontSize: 40,
+      textDirection: TextDirection.ltr,
+      maxLines: 1,
+    ));
+
+    builder.pushStyle(ui.TextStyle(
+        color: Colors.black87, textBaseline: ui.TextBaseline.alphabetic));
+    builder.addText("Hello World");
+    ui.Paragraph paragraph = builder.build();
+    paragraph.layout(ui.ParagraphConstraints(width: 300));
+    canvas.drawParagraph(paragraph, Offset.zero);
+    canvas.drawRect(Rect.fromLTRB(0, 0, 300, 40),
+        Paint()..color = Colors.blue.withOpacity(0.5));
+  }
+
+  /// TextPainter获取文字范围
+  drawTextPaintShowSize(Canvas canvas, Size size) {
+    canvas.translate(size.width / 2, size.height / 2);
+    TextPainter textPainter = TextPainter(
+        text: TextSpan(
+            text: "Hello World",
+            style: TextStyle(fontSize: 40, color: Colors.black)),
+        textAlign: TextAlign.center,
+        textDirection: TextDirection.ltr);
+    ///进行布局
+    textPainter.layout();
+    Size textSize = textPainter.size;
+    Log.info('textSize: $textSize', StackTrace.current);
+    textPainter.paint(canvas, Offset(-textSize.width / 2, -textSize.height / 2));
+    canvas.drawRect(Rect.fromLTRB(0, 0, textSize.width, textSize.height).translate(-textSize.width / 2, -textSize.height / 2), Paint()..color = Colors.blue.withAlpha(33));
+
+    TextPainter sizeTextPainter = TextPainter(
+      text: TextSpan(text: textSize.toString(), style: TextStyle(fontSize: 30, color: Colors.green)),
+      textAlign: TextAlign.center,
+      textDirection: TextDirection.ltr
+    );
+    sizeTextPainter.layout();
+    sizeTextPainter.paint(canvas, Offset(-100, 100));
   }
 }
